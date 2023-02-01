941173f595a3921c8ba24c810d2e47955dddbf86
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-01 12:01:41 -0600

    Updated the file by adding and combining code

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
-// fetch('https://tarot-api.onrender.com/api/v1/cards')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Error:', error))
-
 fetch('https://tarot-api.onrender.com/api/v1/cards')
-  .then(response => response.json())
-  .then(data => {
-      const card1 = document.getElementById("card");
-      if (data.length <= 0) {
-          card1.innerHTML += `${'No cards'}`;
-      } else {
-                for (cards in data) {
-                    //console.log(data[cards[i].value]);
-                    card1.innerHTML += `${data[cards]}<br>`;
-}
-}
-})
-.catch(error => console.error('Error:', error))
+    .then(response => response.json())
+    .then(data => {
+        const card1 = document.getElementById("card");
+        if (data.cards.length <= 0) {
+            card1.innerHTML += `${'No cards'}`;
+        } else {
+                    let html = "";
+                    for (let i = 0; i < data.cards.length; i++) {
+                        html += `<div class="card">
+                    <h2>${data.cards[i].name}</h2>
+                    </div>` //Using a div to style each card 
+                        console.log(data.cards[i]);
+                    }
+                    const result = document.getElementById("result");
+                    result.innerHTML = html;
+
+
+        }
+    })
+    .catch(error => console.error('Error:', error))
 
 
 
