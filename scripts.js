45905c60275a63b7f7548f5b3d72c71164ea6369
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-02 14:13:50 -0600

    Updated the file by adding the pictures of the cards and applying some style and functionality to the file

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
-fetch('https://tarot-api.onrender.com/api/v1/cards')
+//Using a button to fetch API
+document.addEventListener("DOMContentLoaded", (event) => {
+    const getCardBtn = document.getElementById("getCardBtn");
+    getCardBtn.addEventListener("click", (event) => {
+        fetchData('https://tarot-api.onrender.com/api/v1/cards/random?n=3') //This url fetches 3 cards only
+    });
+});
+//Fetch function 
+function fetchData(url) {
+    fetch(url)
     .then(response => response.json())
     .then(data => {
-        const card1 = document.getElementById("card");
-        if (data.cards.length <= 0) {
-            card1.innerHTML += `${'No cards'}`;
-        } else {
-                    let html = "";
-                    for (let i = 0; i < data.cards.length; i++) {
-                        html += `<div class="card">
-                    <h2>${data.cards[i].name}</h2>
-                    </div>` //Using a div to style each card 
-                        console.log(data.cards[i]);
-                    }
-                    const result = document.getElementById("result");
-                    result.innerHTML = html;
-
-
+        let html = "";
+        for (let i=0; i < data.cards.length; i++) {
+            html += `<div class="card-section">
+            <h2>${data.cards[i].name}</h2>
+            <li ><img class="card-li" src="cards_img/${data.cards[i].name_short}.jpg"></li>
+            </div>` //Using a div to style each card 
+            console.log(data.cards[i]);
         }
+    const result = document.getElementById("result");
+    result.innerHTML = html;
     })
-    .catch(error => console.error('Error:', error))
-
-
+}
 
 
 
