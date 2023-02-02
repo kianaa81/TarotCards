14b8c81c3a4e77b1b24ac15b57d2ac442b7dd928
Iktan <delav1004@gmail.com>
2023-02-02 11:31:59 -0600

    Code to include images to each card. Using ${data.cards[i].name_short} to assign the correct image to each card.

--- scripts.js
+++ scripts.js
@@ -14,6 +14,7 @@ function fetchData(url) {
         for (let i=0; i < data.cards.length; i++) {
             html += `<div class="card">
             <h2>${data.cards[i].name}</h2>
+            <img src="cards_img/${data.cards[i].name_short}.jpg">
             </div>` //Using a div to style each card 
             console.log(data.cards[i]);
         }
